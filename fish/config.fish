b0be718438249155cd1ad112594604e94aaa87d7
jacobragsdale <jacobragsdale@protonmail.com>
2021-04-28 18:59:11 -0700

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+# remove the fish greeting
+set fish_greeting
+
+# set the prompt to starship
+starship init fish | source
+
+# set aliases
+alias install="paru -Syy"
+alias uninstall="paru -Rns"
+alias update="paru -Syyuu"
+alias off="sudo shutdown 0"
+alias reboot="sudo reboot"
+alias bt="exec /home/jacob/Development/bt/target/debug/bt"
+alias yt="exec /home/jacob/Development/yt/target/debug/yt"
+
+#env variables
+export set MICRO_TRUECOLOR=1
