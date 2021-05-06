1127838b5380a69a4e3fb36697580dd1177b79d3
jacobragsdale <jacobragsdale@protonmail.com>
2021-05-06 16:32:44 -0700

    added lemonbar script

---
+++ lemonbar/lemonbar.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+#Define the clock
+Clock() {
+        DATETIME=$(date "+%a %b %d, %I:%M:%S %p")
+        echo -n "$DATETIME"
+}
+
+#Define the battery
+Battery() {
+        BATPERC=$(cat /sys/class/power_supply/BAT0/capacity)
+        echo "BAT $BATPERC%"
+}
+
+#Define the network
+Network() {
+        NETWORK=$(nmcli -t -f NAME connection show --active)
+        echo "$NETWORK"
+}
+
+#Define the CPU usage
+Cpu() {
+        CPU=$(top -bn1 | grep "Cpu(s)" | \
+           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
+           awk '{print 100 - $1}' | xargs printf "%.0f")
+        echo "CPU $CPU%"
+}
+
+#Define the RAM usage
+Ram() {
+        RAM=$(free | grep Mem | awk '{print $3/$2 * 100.0}' | xargs printf "%.0f")
+        echo "RAM $RAM%"
+}
+
+# Print the clock
+while true; do
+        echo " $(Network) %{c}$(Clock) %{r}$(Battery) $(Cpu) $(Ram) "
+        sleep 1
+done
